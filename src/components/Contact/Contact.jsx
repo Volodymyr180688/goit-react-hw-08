17127728aacc58852566f219f664931d124f1d0a
Volodymyr180688 <shurik180688@gmail.com>
2024-06-13 23:21:24 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/contactsOps';
+import { deleteContact } from '../../redux/contacts/operations';
 import { HiUser, HiPhone } from 'react-icons/hi';
 import style from './Contact.module.css';
 
